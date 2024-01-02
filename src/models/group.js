3bcc4080ad757abfe018ba57b4f3fd798d28f751
Trần Văn Khang <61522659+NotHadesss@users.noreply.github.com>
2024-01-02 19:45:10 +0700

    Defined associations

--- src/models/group.js
+++ src/models/group.js
@@ -11,6 +11,9 @@ module.exports = (sequelize, DataTypes) => {
          */
         static associate(models) {
             // define association here
+            Group.hasMany(models.User);
+            Group.belongsToMany(models.Role, { through: 'GroupRole' });
+
         }
     };
     Group.init({
