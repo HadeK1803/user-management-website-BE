d4019be26f9f9d26580f877b9e6517497840994e
Trần Văn Khang <61522659+NotHadesss@users.noreply.github.com>
2024-01-02 17:36:59 +0700

    Created all Models for the project

--- src/models/groupRole.js
+++ src/models/groupRole.js
@@ -3,7 +3,7 @@ const {
     Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-    class User extends Model {
+    class GroupRole extends Model {
         /**
          * Helper method for defining associations.
          * This method is not a part of Sequelize lifecycle.
@@ -13,19 +13,14 @@ module.exports = (sequelize, DataTypes) => {
             // define association here
         }
     };
-    User.init({
-        email: DataTypes.STRING,
-        username: DataTypes.STRING,
-        password: DataTypes.STRING,
-        address: DataTypes.STRING,
-        phone: DataTypes.STRING,
-        sex: DataTypes.BOOLEAN,
-        groupId: DataTypes.INTEGER
+    GroupRole.init({
+        groupId: DataTypes.INTEGER,
+        roleId: DataTypes.INTEGER
 
 
     }, {
         sequelize,
-        modelName: 'User',
+        modelName: 'GroupRole',
     });
-    return User;
+    return GroupRole;
 };
--- src/models/projectUser.js
+++ src/models/projectUser.js
@@ -3,7 +3,7 @@ const {
     Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-    class User extends Model {
+    class ProjectUser extends Model {
         /**
          * Helper method for defining associations.
          * This method is not a part of Sequelize lifecycle.
@@ -13,19 +13,14 @@ module.exports = (sequelize, DataTypes) => {
             // define association here
         }
     };
-    User.init({
-        email: DataTypes.STRING,
-        username: DataTypes.STRING,
-        password: DataTypes.STRING,
-        address: DataTypes.STRING,
-        phone: DataTypes.STRING,
-        sex: DataTypes.BOOLEAN,
-        groupId: DataTypes.INTEGER
+    ProjectUser.init({
+        groupId: DataTypes.INTEGER,
+        userId: DataTypes.INTEGER
 
 
     }, {
         sequelize,
-        modelName: 'User',
+        modelName: 'ProjectUser',
     });
-    return User;
+    return ProjectUser;
 };
