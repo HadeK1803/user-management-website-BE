a33d0051658065eb6649dba4b9fc290bfcc83078
Trần Văn Khang <61522659+NotHadesss@users.noreply.github.com>
2024-01-02 20:52:57 +0700

    test associations

--- src/models/group.js
+++ src/models/group.js
@@ -12,7 +12,7 @@ module.exports = (sequelize, DataTypes) => {
         static associate(models) {
             // define association here
             Group.hasMany(models.User);
-            Group.belongsToMany(models.Role, { through: 'GroupRole' });
+            Group.belongsToMany(models.Role, { through: 'Group_Role' });
 
         }
     };
--- src/services/userService.js
+++ src/services/userService.js
@@ -30,19 +30,67 @@ const createNewUser = async (email, username, password) => {
     }
 }
 const getUserList = async () => {
-    // // create the connection, specify bluebird as Promise
-    // const connection = await mysql.createConnection({ host: 'localhost', user: 'root', database: 'user_management', Promise: bluebird });
+    /**
+     // create the connection, specify bluebird as Promise
+     const connection = await mysql.createConnection({ host: 'localhost', user: 'root', database: 'user_management', Promise: bluebird });
+ 
+     try {
+         // query database
+         const [rows, fields] = await connection.execute('SELECT * FROM users');
+         console.log("Got all users successfully!");
+ 
+         return rows;
+     } catch (err) {
+         console.log(">>> Error message: " + err.message);
+         return [];
+     }
+    */
+    try {
+        /**
+         * Normally we have group 1 and we will find all roles in that group
+         * => But when we code, we will find all role from group 1
+         */
+        // From Group 1 to find all roles 
+        // let roles = await db.Group.findAll({
+        //     where: { id: 1 },
+        //     include: {
+        //         model: db.Role,
+        //     },
+        //     raw: true,
+        //     nest: true,
+        // });
+        // Find all roles from group 1
+        let roles = await db.Role.findAll({
+            include: {
+                model: db.Group,
+                where: { id: 1 },
+                attributes: ["id", "name", "description"],
 
-    // try {
-    //     // query database
-    //     const [rows, fields] = await connection.execute('SELECT * FROM users');
-    //     console.log("Got all users successfully!");
+            },
+            attributes: ["id", "url", "description"],
+            raw: true,
+            nest: true,
+        });
 
-    //     return rows;
-    // } catch (err) {
-    //     console.log(">>> Error message: " + err.message);
-    //     return [];
-    // }
+        let arr = await db.User.findAll({
+            include: {
+                model: db.Group,
+                where: { id: 1 },
+                attributes: ["id", "name", "description"],
+            },
+            attributes: ["id", "username", "email"],
+            raw: true,
+            nest: true,
+        });
+        // console.log(">>>Check users: ", arr);
+
+        console.log(">>>Check roles: ", roles);
+    }
+    catch (err) {
+        console.log(">>>Error: ", err);
+    }
+
+    //----------------------------------------------------------------
     let users = [];
 
     users = await db.User.findAll();
